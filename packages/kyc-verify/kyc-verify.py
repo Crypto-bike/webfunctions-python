a976b607e7da64fd388a072a3670f9b306ed25c6
Crypto-bike <106312111+Crypto-bike@users.noreply.github.com>
2023-08-11 09:54:40 +0300

    Update kyc-verify.py
    Signed-off-by: Crypto-bike <106312111+Crypto-bike@users.noreply.github.com>

--- packages/kyc-verify/kyc-verify.py
+++ packages/kyc-verify/kyc-verify.py
@@ -1,5 +1,16 @@
+import requests
+
 def main(args):
-      name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!"
-      print(greeting)
-      return {"body": greeting}
+      code = args.get("code")
+      backend_url = 'https://dev.back-ng.crypto-bike.game/account/verifyMail/' + code + '/'
+      response = requests(backend_url)
+      if (response.status_code == 200):
+            with open('success.html', 'r') as f:
+                  html_string = f.read()
+                  return {"body": html_string}
+      else:
+            with open('error.html', 'r') as f:
+                  html_string = f.read()
+                  return {"body": html_string}
+
+      
