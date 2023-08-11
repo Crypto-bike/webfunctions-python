a40c1fe13145cbebd0ace8b2842249de4b9b4709
Crypto-bike <106312111+Crypto-bike@users.noreply.github.com>
2023-08-11 12:01:33 +0300

    Update __main__.py
    Signed-off-by: Crypto-bike <106312111+Crypto-bike@users.noreply.github.com>

--- packages/python-serverless/kyc-verify/__main__.py
+++ packages/python-serverless/kyc-verify/__main__.py
@@ -5,12 +5,11 @@ def main(event):
       backend_url = 'https://dev.back-ng.crypto-bike.game/account/verifyMail/' + code + '/'
       response = requests(backend_url)
       if (response.status_code == 200):
-            with open('success.html', 'r') as f:
-                  html_string = f.read()
-                  return {"body": html_string}
-      else:
-            with open('error.html', 'r') as f:
-                  html_string = f.read()
-                  return {"body": html_string}
+            with open('success.html', 'r') as file:
+                  html_string = file.read()
+			return {"body": f'{html_string}'}
 
-      
+      else:
+            with open('error.html', 'r') as file:
+                  html_string = file.read()
+                  return {"body": f'{html_string}'}
