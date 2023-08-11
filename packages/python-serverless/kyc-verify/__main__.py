1d084c56164e41e04ef2113a89b69b5dedf5711a
Crypto-bike <106312111+Crypto-bike@users.noreply.github.com>
2023-08-11 12:52:24 +0300

    123

--- packages/python-serverless/kyc-verify/__main__.py
+++ packages/python-serverless/kyc-verify/__main__.py
@@ -1,14 +1,12 @@
 import requests
 
 def main(event):
-      code = event.get("code")
-      backend_url = 'https://dev.back-ng.crypto-bike.game/account/verifyMail/' + code + '/'
-      response = requests.get(backend_url)
+      code = event.get("code")]
+      response = requests.get(f'https://dev.back-ng.crypto-bike.game/account/verifyMail/{code}')
       if (response.status_code == 200):
             with open('success.html', 'r') as file:
                   html_string = file.read()
 			return {"body": f'{html_string}'}
-
       else:
             with open('error.html', 'r') as file:
                   html_string = file.read()
--- packages/python-serverless/preregistration-verify/__main__.py
+++ packages/python-serverless/preregistration-verify/__main__.py
@@ -2,13 +2,11 @@ import requests
 
 def main(event):
       code = event.get("code")
-      backend_url = 'https://dev.back-ng.crypto-bike.game/preregistration/' + code + '/'
-      response = requests(backend_url)
+      response = requests.get(f'https://dev.back-ng.crypto-bike.game/preregistration/{code}')
       if (response.status_code == 200):
             with open('success.html', 'r') as file:
                   html_string = file.read()
 			return {"body": f'{html_string}'}
-
       else:
             with open('error.html', 'r') as file:
                   html_string = file.read()
