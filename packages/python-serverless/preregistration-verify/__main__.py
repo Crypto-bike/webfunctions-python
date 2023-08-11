1d3b1c2cc2e6571b676f0f266f5e93131640937e
Crypto-bike <106312111+Crypto-bike@users.noreply.github.com>
2023-08-11 13:49:36 +0300

    123

--- packages/python-serverless/preregistration-verify/__main__.py
+++ packages/python-serverless/preregistration-verify/__main__.py
@@ -1,12 +1,13 @@
 import requests
 
 def main(event):
-      code = event.get("code")
-      response = requests.get(f'https://dev.back-ng.crypto-bike.game/preregistration/{code}')
+      code = event.get("code", "null")
+      console
+      response = requests.get(url=f'https://dev.back-ng.crypto-bike.game/preregistration/{code}')
       if (response.status_code == 200):
             with open('success.html', 'r') as file:
                   html_string = file.read()
-			return {"body": f'{html_string}'}
+                  return {"body": f'{html_string}'}
       else:
             with open('error.html', 'r') as file:
                   html_string = file.read()
